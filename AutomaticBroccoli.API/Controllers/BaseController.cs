3f42aa9e3d8c4d645d79eb0ffb86ae1a1b717e7a
Дмитрий Горбунов <bous07@mail.ru>
2024-02-11 20:32:12 +0300

    expose base controller with basic attributes

---
+++ AutomaticBroccoli.API/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace AutomaticBroccoli.API.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public abstract class BaseController : ControllerBase
+    {
+
+    }
+}
